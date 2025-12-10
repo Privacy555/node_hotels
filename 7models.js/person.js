f3602cf673a0ef821e0e9cebe7d117dd1bfeab66
Privacy555 <business.web369@gmail.com>
2025-12-10 13:56:20 +0545

    hashed encryption enabled

--- 7models.js/person.js
+++ 7models.js/person.js
@@ -1,5 +1,7 @@
 const mongoose=require('mongoose');
 
+const bcrypt=require('bcrypt');
+
 const personSchema=new mongoose.Schema({
     name:{
         type:String,
@@ -28,9 +30,47 @@ const personSchema=new mongoose.Schema({
     salary:{
         type:String,
         required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
     }
 });
 
+//Encrypting pwd      (When using async middleware, you must NOT use next().)
+personSchema.pre("save",async function(){                                   //arrow function ()=>{} doesn't give this because arrow functions do not bind their own this.
+    const person=this;
+    try{
+        //hash the pwd if it has been modified or new
+        if(!person.isModified('password')) return;
+
+        //salt generation
+        const salt=await bcrypt.genSalt(10);
+        
+        const hashedPassword= await bcrypt.hash(person.password,salt);
+        
+        //override the plain password with hashed password 
+        person.password=hashedPassword;
+
+    }catch(err){
+        //return err;                     //return error does nothing
+        throw err;
+    }
+})
+
+personSchema.methods.comparePassword=async function (candidatePassword) {                                   //personSchema.methods.comparePassword = ... means you are adding a custom method to all Person documents created from your Mongoose schema.
+    try{
+        //use bcrypt to compare the provided password with the hashed password
+        const isMatch= await bcrypt.compare(candidatePassword,this.password);               //returns true or false. we must add await here ,otherwise it won't wait for that function's result . that might cause in login with wrong credentials
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
 
 //create a person model
 const Person=mongoose.model('Person',personSchema);
--- 8serverwithRouting.js
+++ 8serverwithRouting.js
@@ -1,6 +1,22 @@
 const express=require('express');
 const app=express();
 
+const passport=require('./auth');
+
+
+//middleware function   (keep it before routing files)
+const logRequest=(req,res,next)=>{
+    console.log("middleware reached");
+    console.log(`${new Date().toLocaleString()} request made to: ${req.originalUrl}`);
+    next();                     //signal to next js that the current middleware function is completed and it's time to move on next middleware function  or route handler in the chain.
+}
+app.use(logRequest);
+
+
+app.use(passport.initialize());
+const authenticateMiddleware=passport.authenticate('local',{session:false});      //to access Person data , the user should have id and password in person collection.(i.e the authentication logic is made on username password of user in Person collection.)
+
+
 //importing db(mongoose one) for connection with server
 const db=require('./7db')
 
@@ -14,23 +30,20 @@ const PORT=process.env.PORT || 3000                     //if there is no PORT in
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());                                             //stores in req.body        ,       since we are using for json file,so we have include .json()
 
-     
-
-
 
 
 
 // pathway for personRoutes
 const personRoutes=require('./routes/personRoutes');
-app.use('/person',personRoutes);                                        //we use person here cuz person was common in each path of  get ,post apks
+app.use('/person',authenticateMiddleware,personRoutes);                                        //we use person here cuz person was common in each path of  get ,post apks
 
 
 //pathway for menuRoutes
 const menuRoutes=require('./routes/menuRoutes');
-app.use('/menu',menuRoutes);
+app.use('/menu',menuRoutes);                   
 
 
-app.get('/',(req,res)=>{
+app.get('/',function(req,res){
     res.send("Welcome to my hotel. how can i help you? We have list of menus.");
 })
 
@@ -39,3 +52,4 @@ app.get('/',(req,res)=>{
 app.listen(PORT,()=>{
     console.log("Server started on port 3000")
 })
+
